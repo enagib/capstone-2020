191ce1ab0c1c4ac4e5331629ea398b84a04f9ea9
enagib <enagib@bu.edu>
2020-03-14 14:52:02 -0400

    few changes

--- Tweets and Sentiment Analysis/Airline-Tweets.R
+++ Tweets and Sentiment Analysis/Airline-Tweets.R
@@ -1,13 +1,15 @@
 library(tidyr)
+library(tidytext)
 library(tibble)
 library(dplyr)
 library(lubridate)
 library(ggplot2)
 library(rtweet)
-
-## install rtweet from CRAN
-install.packages("rtweet")
+library(stringr)
+library(quanteda)
+library(wordcloud)
 library(rtweet)
+library(topicmodels)
 
 # get_timeline() - Get one or more user timelines (tweets posted by target user(s))
 # get_favorites() - Get tweets data for statuses favorited by one or more target users.
@@ -56,7 +58,85 @@ maps::map("state", lwd = .25)
 # plot lat and lng points onto state map
 with(alaska_rt, points(lng, lat, pch = 20, cex = .75, col = rgb(0, .3, .7, .75)))
 
+##########SENTIMENT ANALYSIS###############
+alaska_text <- alaska_tweets %>% select(status_id, text)
+tidy_alaska <- alaska_text %>% unnest_tokens(token, text, token = "tweets", strip_url = T, strip_punct = T, to_lower = T)
+tidy_alaska_sw <- tidy_alaska %>% anti_join(get_stopwords(), by=c("token" = "word"))
 
+sw = get_stopwords()
+sw = c(sw$word, "@alaskaair", "alaskaair")
+
+tidy_alaska2 = tidy_alaska_sw %>% filter(!token %in% sw)
+tidy_alaska2 %>% count(token, sort=T) %>% top_n(15) %>% 
+  ggplot(aes(reorder(token, n), n)) +
+  geom_col() +
+  coord_flip()
+
+alaska_bing <- inner_join(tidy_alaska2, get_sentiments("bing"), by=c("token" = "word"))
+head(alaska_bing)
+
+# negative versus positive sentiment words and their frequency. 
+alaska_positive = alaska_bing %>% filter(sentiment == "positive") %>% 
+  count(token, sort=T) %>% top_n(10) %>% 
+  ggplot(aes(reorder(token, n), n)) +
+  geom_col() +
+  xlab("Top 10 Words") +
+  ggtitle("Top Positive Words") +
+  coord_flip() 
+
+alaska_negative = alaska_bing %>% filter(sentiment == "negative") %>% 
+  count(token, sort=T) %>% top_n(10) %>% 
+  ggplot(aes(reorder(token, n), n)) +
+  geom_col() +
+  xlab("Top 10 Words") +
+  ggtitle("Top Negative Words") +
+  coord_flip()
+
+gridExtra::grid.arrange(alaska_positive, alaska_negative , nrow = 1)
+
+## Topic modelling
+alaska_corpus <- corpus(alaska_text$text)
+alaska_dfm = dfm(alaska_corpus,
+              remove_punct = T,
+              remove = stopwords(),
+              remove_numbers = T,
+              remove_url = T,
+              remove_symbols = T) %>% 
+  dfm_trim(min_termfreq = 10, 
+           termfreq_type = "count",
+           max_docfreq = .65,
+           docfreq_type = "prop")
+
+dtm = convert(alaska_dfm, to = "topicmodels")
+
+crm_topics = LDA(dtm, 5, control = list(seed=820))
+crm_topics
+terms(crm_topics, 10)
+
+crm_beta = tidy(crm_topics, matrix="beta")
+
+crm_top10 = crm_beta %>% 
+group_by(topic) %>% 
+  top_n(10, beta) %>% 
+  ungroup() %>% 
+  arrange(desc(beta)) 
+
+crm_top10 %>% 
+  mutate(term = reorder_within(term, beta, topic))  %>% 
+  ggplot(aes(term, beta)) +
+  geom_col() +
+  facet_wrap(~ topic, scales="free") +
+  coord_flip() +
+  scale_x_reordered()
+
+### gamma - mixtures of topics across documents
+crm_gamma = tidy(crm_topics, matrix="gamma")
+crm_gamma %>% arrange(-gamma) %>% print(n=10)
+
+
+## top tokens
+textstat_frequency(alaska_dfm, n = 10)
+textplot_wordcloud(alaska_dfm, min_count = 30)
 
 
 ########################################################################
